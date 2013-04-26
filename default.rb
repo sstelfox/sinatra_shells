ff868b60688c5b2edf5f3392a5a32efb89e4c86e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-04-26 17:30:24 -0400

    Think that's all I need to do...

--- default.rb
+++ default.rb
@@ -14,6 +14,31 @@ require 'lib/init_database'
 
 require 'lib/scss_engine'
 
+class Shell
+  def close
+    @read_socket.close
+    @write_socket.close
+    Process.waitpid(@pid)
+  end
+
+  def initialize(ws)
+    @read_socket, @write_socket, @pid = PTY.spawn('env PS1="[\u@\h] \w\$ " TERM=xterm-256color COLUMNS=80 LINES=24 sh -i')
+    @websocket = ws
+    ObjectSpace.define_finalizer(self, method(:close))
+  end
+
+  def send(msg)
+    @write_socket.puts(msg)
+    while IO.select([@read_socket], nil, nil, 0])
+      websocket.send(@read_socket.read_nonblock(1024))
+    end
+  end
+
+  def websocket
+    @websocket
+  end
+end
+
 module Default
   class App < Sinatra::Base
     enable :logging, :sessions, :method_override
@@ -21,6 +46,8 @@ module Default
     set :root, File.expand_path(File.dirname(__FILE__))
     set :views, (self.root + '/views')
     set :public_folder, (self.root + '/public')
+
+    set :shells, []
 
     configure :development do
       enable :raise_errors
@@ -35,6 +62,22 @@ module Default
     # Available methods: OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
     get '/' do
       erb :index
+    end
+
+    get '/shells' do
+      pass unless request.websocket?
+
+      request.websocket do |ws|
+        ws.onopen do
+          settings.shells << Shell.new(ws)
+        end
+        ws.onmessage do |msg|
+          EM.next_tick { settings.shells.each{ |s| s.send(msg) } }
+        end
+        ws.onclose do
+          settings.shells.delete_if { |s| s.websocket == ws }
+        end
+      end
     end
 
     not_found do
